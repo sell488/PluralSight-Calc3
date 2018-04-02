9c5489aba3d5fa6d7fe3d3c033be99175eca6b76
sell488 <sell488sftoday@gmail.com>
2018-04-02 12:51:30 -0500

    inheritance

--- src/com/pluralsight/calc2/Main.java
+++ src/com/pluralsight/calc2/Main.java
@@ -38,6 +38,23 @@ public class Main {
         System.out.print("result=");
         System.out.println(equationOverload.getResult());
 
+
+        System.out.println();
+        System.out.println("Using Inheritance");
+        System.out.println();
+
+        CalculateBase [] calculators = {
+          new Divider(100.0d, 50.0d),
+          new Adder(25.0d, 92.0d),
+          new Subtracter(225.0d, 17.0d),
+          new Multiplier(11.0d, 3.0d)
+        };
+
+        for(CalculateBase calculator : calculators) {
+            calculator.calculate();
+            System.out.println("result = ");
+            System.out.println(calculator.getResult());
+        }
     }
 
 }
